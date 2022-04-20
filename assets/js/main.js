1883831fa4d00fac6deb16637d4c98d06b105108
liamsegura <liamdiegosegura@gmail.com>
2022-04-20 18:06:18 +0100

    Added extra choice js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,16 @@
-['heads', 'tails'].forEach(choice => document.getElementById(choice).addEventListener('click', () => headsOrTails(choice)))
-function headsOrTails(choice){
-    const randomChoice = Math.random() < .5 ? "heads" : "tails"
-    console.log(
-        choice == randomChoice ? `You win! it landed on ${randomChoice}` : `You lose! it landed on ${randomChoice}`
-    )
+['rock','paper','scissors'].forEach(choice => document.getElementById(choice).addEventListener('click', () => playerChoice(choice)))
+
+function botChoice(){
+    return Math.random() < .3 ? 'rock'
+         : Math.random() < .6 ? 'paper' : 'scissors'
+}
+
+function playerChoice(choice){
+
+    const botsChoice = botChoice()
+    const rules = {rock: "scissors", paper: "rock", scissors: "paper"}
+    console.log(botsChoice == rules[choice] ? `You win! your oppenent chose ${botsChoice}`
+    : choice == botsChoice ? `You draw! you both chose ${choice}` 
+    : `You lose! your oppenent chose ${botsChoice}`)
+
 }
